840a0e22fcd0dfed580e0cd13e846ead6bcf90fa
yamakei0727 <yamakei0727@gmail.com>
2025-02-15 15:07:02 +0900

    ligoデプロイ

--- db_control/connect_MySQL.py
+++ db_control/connect_MySQL.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 # 環境変数の読み込み
 base_path = Path(__file__).parents[1]  # backendディレクトリへのパス
-# env_path = base_path / '.env'
-# load_dotenv(dotenv_path=env_path)
+env_path = base_path / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # データベース接続情報
 DB_USER = os.getenv('DB_USER')
